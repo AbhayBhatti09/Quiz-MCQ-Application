cfb62cf38887826597948823580d530164e7a75d
AbhayBhatti09 <abhayb.albiorix@gmail.com>
2025-12-03 13:56:14 +0530

    changes vita file

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,17 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    server: {
-        host: "0.0.0.0", // Important: listen to all devices
-        port: 5173, // Vite default port
-        strictPort: true,
-        hmr: {
-            host: "192.168.1.42", // Your IP here
-            protocol: "ws",
-            port: 5173,
-        },
-        cors: true, // <-- solves CORS error
-    },
+    // server: {
+    //     host: "0.0.0.0", // Important: listen to all devices
+    //     port: 5173, // Vite default port
+    //     strictPort: true,
+    //     hmr: {
+    //         host: "192.168.1.42", // Your IP here
+    //         protocol: "ws",
+    //         port: 5173,
+    //     },
+    //     cors: true, // <-- solves CORS error
+    // },
 
     plugins: [
         laravel({
