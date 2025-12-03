22578e81ec0468af618d22386c62e928d9309340
AbhayBhatti09 <abhayb.albiorix@gmail.com>
2025-12-03 14:27:03 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,6 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    // server: {
-    //     host: "0.0.0.0", // Important: listen to all devices
-    //     port: 5173, // Vite default port
-    //     strictPort: true,
-    //     hmr: {
-    //         host: "192.168.1.42", // Your IP here
-    //         protocol: "ws",
-    //         port: 5173,
-    //     },
-    //     cors: true, // <-- solves CORS error
-    // },
-
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
