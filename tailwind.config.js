cc9a178e99f4abfa4866682d6d083ced29c9a1e4
AbhayBhatti09 <abhayb.albiorix@gmail.com>
2025-12-11 18:12:44 +0530

    tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.js", // ← include JS also
+    ],
+
+    safelist: [
+        // Status box colors
+        "bg-red-100",
+        "bg-green-200",
+
+        // Grid layout for question numbers
+        "grid",
+        "grid-cols-5",
+        "gap-2",
+        "p-2",
+
+        // If you use more:
+        // 'bg-yellow-200',
+        // 'bg-blue-200',
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
